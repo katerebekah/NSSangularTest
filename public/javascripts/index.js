1a8070101548a444f9fc479b7f8cd64506ffca9d
Kate <katerebekah@gmail.com>
2015-05-23 09:32:42 -0500

    making a single page app today -- setup

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,11 +1,7 @@
 (function() {
   'use strict';
-  angular.module('hallo', []).controller('mainController', ['$scope', function($scope){
-  		$scope.name = 'Kate';
-  		$scope.instructors = [
-  			{name: 'Steve', allergies: 'sunshine'},
-  			{name: 'Zoe', allergies: 'students'},
-  			{name: 'Joe', allergies: "everything"}
+  angular.module('kateApp', []).controller('kateController', ['$scope', function($scope){
+
   			];
 
  		}]);
