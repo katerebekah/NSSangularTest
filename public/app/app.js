4a9642e65519ba5e1aeb418701e1255f81f9541b
Kate <katerebekah@gmail.com>
2015-05-23 14:00:32 -0500

    sigh

--- public/app/app.js
+++ public/app/app.js
@@ -4,6 +4,8 @@ app.config(['$routeProvider', function($routeProvider){
     $routeProvider
     .when('/', {templateUrl: 'partials/index', controller: 'indexController'})
     .otherwise({redirectTo:'/'})
+    .when('/about', {templateUrl: 'partials/about', controller: 'aboutController'})
+    .when('/needs', {templateUrl: 'partials/needs', controller: 'needsController'})
 }])
 
 .config(['$locationProvider', function($locationProvider){
--- public/app/controllers.js
+++ public/app/controllers.js
@@ -2,4 +2,16 @@ var controllers = angular.module('myApp.controllers', []);
 
 controllers.controller('indexController', ['$scope', function($scope){
 	$scope.message = "Index Controller Checking In";
-}])
+}]);
+
+controllers.controller('aboutController', ['$scope', function($scope){
+	$scope.message = "About Controller Checking In";
+}]);
+
+controllers.controller('needsController', ['$scope', function($scope){
+	$scope.message = "Needs Controller Checking In";
+	$scope.needs = [];
+	$scope.addNeeds= function(need){
+		$scope.needs.push(need);
+		$scope.newNeed = '';}
+}]);
